7bf1443b9a7ac5329b7119a4e7b0cd7360c56cdd
juligarc91 <julissagarcia@pursuit.org>
2024-03-01 16:13:23 -0500

    added 404 page and adjusted package.json

--- app.js
+++ app.js
@@ -16,4 +16,8 @@ app.get('/',(req,res)=>{
 })
 app.use('/transactions', transactionsController);
 
+//404 page
+app.get('*',(req,res) => {
+    res.status(404).json({ error: "Page not found :-(" })
+})
 module.exports = app;
