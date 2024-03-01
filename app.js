a7cd8669094f7446d5da1d932a81c0f63b197bca
juligarc91 <julissagarcia@pursuit.org>
2024-03-01 16:04:19 -0500

    installed cors package to work with front end gui

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // depedencies
 const express = require('express');
+const cors = require('cors');
 // config
 const app = express();
 
+const transactionsController = require('./controllers/logs.controller');
+
+//middleware packges
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.get('/',(req,res)=>{
     res.status(200).json({"message" : "Hello"})
 })
+app.use('/transactions', transactionsController);
 
 module.exports = app;
--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const transactions = express.Router();
 const transactionsData = require("../models/logs.model");
+
+transactions.get('/',(req,res)=>{
+    res.status(200).json({transactions: transactionsData})
+});
+
+module.exports = transactions;
